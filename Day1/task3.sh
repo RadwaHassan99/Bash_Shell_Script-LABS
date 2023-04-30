e177a69460d8101d2b717800b79c56bf19db3ca7
Radwa99 <radwa.prof.9@gmail.com>
2023-04-30 10:47:04 +0300

    edit task3

--- Day1/task3.sh
+++ Day1/task3.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#execute by using root the script every one min to monitor system load and add it to var/log/system-load.PS
+##Exit Codes:
+##0: Normal Terminated
+##1: User isn't root
 
 # Check if script is being run as root
 if [[ $EUID -ne 0 ]]; then
@@ -6,9 +10,20 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Get current date and time
-now=$(date +"%Y-%m-%d %H:%M:%S")
+while true
+do
 
-# Get system load and append it to the system-load log file
-load=$(uptime)
-echo "$now - $load" >> /var/log/system-load
+  #Get the System load average for the last minute
+  LOAD=$(uptime)
+
+  # Get current date and time
+  NOW=$(date +"%Y-%m-%d %H:%M:%S")
+
+  # Get system load and append it to the system-load log file
+  echo "$NOW - $LOAD" >> /var/log/system-load
+
+  #log every 60 seconds
+  sleep 60
+done
+
+exit 0
